308a23de4e8294c0334aa312ac7a2ab6f27921e5
Victor Mwadime <victormwadime@gmail.com>
2025-07-29 00:05:55 +0300

    fix typo

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,18 +8,19 @@ const registerUser = async (req, res) => {
       "SELECT * FROM users WHERE email = $1",
       [email]
     );
+
     if (existingUser.rows.length > 0) {
       return res.status(400).json({ error: "User alredy exists" });
     }
 
     const newUser = await pool.query(
-      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      "INSERT INTO users (full_name, email, password) VALUES ($1, $2, $3) RETURNING *",
       [name, email, password]
     );
 
     res.status(201).json({ user: newUser.rows[0] });
   } catch (error) {
-    console.error(err.message);
+    console.error(error.message);
     res.status(500).json({ error: "Server error" });
   }
 };
