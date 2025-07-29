1df10b0390fb959dcd58c44c64322849b2f4f6a9
Victor Mwadime <victormwadime@gmail.com>
2025-07-29 23:02:14 +0300

    fix typo

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const pool = require("../db/index");
 const jwt = require("jsonwebtoken");
-const bcryt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
