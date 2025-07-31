d4cd0086415043daa67d41d120b5f3538ce452e1
Victor Mwadime <victormwadime@gmail.com>
2025-07-31 23:54:11 +0300

    import verifyToken middleware and create route to test it

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
+const verifyToken = require("../middleware/verifyToken");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/protected", verifyToken, (req, res) => {
+  res
+    .status(200)
+    .json({ message: `Hello user ${req.user.id}, you are authenticated` });
+});
 
 module.exports = router;
